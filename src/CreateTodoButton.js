8fecad9a27d29698d507c9206e072a0911bea37b
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-02-15 15:03:43 -0500

    feature/eventos: se agregan eventos iniciales

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,6 +1,17 @@
 import "./css/CreateTodoButton.css";
 function CreateTodoButton() {
-  return <button className="CreateTodoButton">+</button>;
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={(event) => {
+        console.info("Le dieron click al boton");
+        console.info("event", event);
+        console.info("event", event.target);
+      }}
+    >
+      +
+    </button>
+  );
 }
 
 export { CreateTodoButton };
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,17 @@
 import "./css/TodoSearch.css";
 function TodoSearch() {
-  return <input className="TodoSearch" placeholder="buscar" />;
+  return (
+    <input
+      className="TodoSearch"
+      placeholder="buscar"
+      onChange={(event) => {
+        console.info("Escribiste en el input");
+        console.info("event", event);
+        console.info("event", event.target);
+        console.info("event target value", event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
