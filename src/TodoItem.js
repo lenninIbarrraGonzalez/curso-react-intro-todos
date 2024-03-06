15c9b19ced0dd353d9ba0e1c4e0c30ecea173293
Lennin Ibarra <ing.lenninibarra@gmail.com>
2024-03-06 17:32:34 -0500

    feature/iconos: se implementan iconos y se pasan props entre hijos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,24 @@
 import "./css/TodoItem.css";
+import { CompleteIcon } from "./CompleteIcon";
+import { DeleteIcon } from "./DeleteIcon";
+
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <span
+      {/* <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.onComplete}
       >
         V
-      </span>
+      </span> */}
+      <CompleteIcon completed={props.completed} onComplete={props.onComplete} />
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">X</span>
+      {/* <span className="Icon Icon-delete" onClick={props.onDelete}>
+        X
+      </span> */}
+      <DeleteIcon onDelete={props.onDelete} />
     </li>
   );
 }
